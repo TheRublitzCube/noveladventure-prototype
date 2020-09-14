c056cbcccc4e2bc81a3081a06b3d0f62e6af3b63
TheRublitzCube <troyjaleel@live.com>
2020-09-13 20:34:18 -0700

    Update 09/13/20
    -Made edit to input permission code
    -Added jumping logic
    -Tweaked character speed and accel./decel.
    -Added wall prefab
    -Added tags for player, wall, and hole
    -Tweaked camera size

--- A Novel Adventure Prototype/Assets/Scripts/PlayerControllerScript.cs
+++ A Novel Adventure Prototype/Assets/Scripts/PlayerControllerScript.cs
@@ -7,19 +7,22 @@ public class PlayerControllerScript : MonoBehaviour
 {
     Rigidbody2D rb; //Player's rigid body object
 
-    /*--------Enum for allowing player to do certain actions--------
-        
-        CanDoAll = Player has complete control
-        CanNotJump = Player can move but cannot jump
-        CanNotMove = Player can jump but cannot move
-        CanDoNone = Player input is completely disabled
-
-    */
-    public enum InputState { CanDoAll, CanNotJump, CanNotMove, CanDoNone };
-    public InputState currentInputState;
+    //--------Input Allowances--------//
+    /*
+        0 - All or None
+        1 - Move or Cant Move
+        2 - Jump or Cant Jump
+        3 - Shoot or Cant Shoot
+     */
+    public bool[] inputAllows = { true, true, true, true };
 
     //--------Movement Variables--------//
     public float movementSpeed; //movement speed
+
+    //--------Jumping Variables--------//
+    float airTimeCounter = 0; //time in the air
+    bool grounded = true;
+    bool hasJumped = false;
 
     //--------Shooting Variables--------//
     public GameObject BulletPrefab;
@@ -37,13 +40,13 @@ public class PlayerControllerScript : MonoBehaviour
     void Update()
     {
         // if player can input to some capacity
-        if (currentInputState != InputState.CanDoNone)
+        if (inputAllows[0] == true)
         {
             Move();
 
             if (!coolingDown)
             {
-                if (Input.GetButtonDown("Fire1")) 
+                if (Input.GetButtonDown("Fire1") && inputAllows[3] == true) 
                 {
                     Shoot();
                     coolingDown = true;
@@ -61,18 +64,38 @@ public class PlayerControllerScript : MonoBehaviour
                 }
             }
 
+            if (!grounded)
+            {
+                airTimeCounter -= Time.deltaTime;
+
+                if (airTimeCounter <= 0)
+                {
+                    grounded = true;
+                    hasJumped = false;
+                    inputAllows[3] = true;
+                    Debug.Log("Player has landed");
+                }
+            
+            }
+
         }
     }
     
     //--------Functions for basic character movement--------
     void Move()
     {
-        if (Input.GetButtonDown("Jump")) //if jump buton pressed and can jump
+        if (Input.GetButtonDown("Jump") && inputAllows[2] == true) //if jump buton pressed and can jump
         {
-            Jump();
+            if (!hasJumped)
+            {
+                Jump();
+                hasJumped = true;
+                airTimeCounter = 0.77f;
+                inputAllows[3] = false;
+            }
         }
 
-        if (currentInputState != InputState.CanNotMove) //if player can move
+        if (inputAllows[1] != false) //if player can move
         {
             rb.velocity = new Vector2(movementSpeed * Input.GetAxis("Horizontal"), movementSpeed * Input.GetAxis("Vertical")); //adds movement
         }
@@ -82,31 +105,20 @@ public class PlayerControllerScript : MonoBehaviour
     //Jumping Function
     void Jump()
     {
-        if (currentInputState != InputState.CanNotJump)
-        {
-            //Debug.Log("Player jumped");
-        }
-
-        else
-        {
-            //Debug.Log("Player cannot jump");
-        }
+        grounded = false;
+        Debug.Log("Player has jumped");
     }
 
     //--------Combat Functions--------
     void Shoot()
     {
-        if (Input.GetButtonDown("Fire1") && !coolingDown)
-        {
-            //Debug.Log("Shot fired");
-            Vector3 aimPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //gets mouse position in relation to player
-            aimPos.z = 0;
-            aimPos.Normalize();
+        //Debug.Log("Shot fired");
+        Vector3 aimPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //gets mouse position in relation to player
+        aimPos.z = 0;
+        aimPos.Normalize();
 
-            GameObject bulletInstance = Instantiate(BulletPrefab, transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
-            bulletInstance.GetComponent<Rigidbody2D>().AddForce(aimPos * 1000);
-            Physics2D.IgnoreCollision(bulletInstance.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-
-        }
+        GameObject bulletInstance = Instantiate(BulletPrefab, transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        bulletInstance.GetComponent<Rigidbody2D>().AddForce(aimPos * 1000);
+        Physics2D.IgnoreCollision(bulletInstance.GetComponent<Collider2D>(), GetComponent<Collider2D>());
     }
 }
